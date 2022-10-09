398f5b8bbb35cbfea91d24d0ad1efdbfd0527db2
Patrick Haas <patrick.haas@gmx.net>
2022-10-09 11:28:45 +0200

    extracted architecture tests.

--- src/test/java/de/ph/example/employees/EmployeesApplicationTests.java
+++ src/test/java/de/ph/example/employees/EmployeesApplicationTests.java
@@ -1,28 +1,14 @@
 package de.ph.example.employees;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeesApplicationTests {
 
-    private final JavaClasses classes = new ClassFileImporter().importPackages(
-            "de.ph.example.employees.domain");
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void onionArchitectureIsRespected() {
-        Architectures.onionArchitecture()
-                .domainModels("de.ph.example.employees.domain")
-                .domainServices("de.ph.example.employees.application")
-                .adapter("de.ph.example.employees.infrastructure")
-                .evaluate(classes);
     }
 
 }
---
+++ src/test/java/de/ph/example/employees/EmployeesArchitectureTests.java
@@ -0,0 +1,21 @@
+package de.ph.example.employees;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.library.Architectures;
+import org.junit.jupiter.api.Test;
+
+public class EmployeesArchitectureTests {
+
+    private final JavaClasses classes = new ClassFileImporter().importPackages(
+            "de.ph.example.employees");
+
+    @Test
+    void onionArchitectureIsRespected() {
+        Architectures.onionArchitecture()
+                .domainModels("de.ph.example.employees.domain")
+                .domainServices("de.ph.example.employees.application")
+                .adapter("de.ph.example.employees.infrastructure")
+                .evaluate(classes);
+    }
+}
